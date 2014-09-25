18465e17a0bc4e0cc00a2289bc64105c89a4b197
tkmagesh <tkmagesh77@gmail.com>
2014-09-25 09:26:28 +0530

    Functional Programming

---
+++ Day-02/01-FunctionanlProgrammingTechniques/products.js
@@ -0,0 +1,8 @@
+var products = [
+    {id : 8, name : "Pen", cost : 60, units : 10, category : 2},
+    {id : 4, name : "Hen", cost : 40, units : 70, category : 1},
+    {id : 7, name : "Ten", cost : 90, units : 40, category : 2},
+    {id : 2, name : "Den", cost : 30, units : 80, category : 1},
+    {id : 3, name : "Len", cost : 50, units : 30, category : 2},
+    {id : 6, name : "Zen", cost : 70, units : 20, category : 1}
+]
