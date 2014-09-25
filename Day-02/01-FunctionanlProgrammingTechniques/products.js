21bc534c0881e5ddc3d9d9e1e40444c0731c4e0c
tkmagesh <tkmagesh77@gmail.com>
2014-09-25 09:52:08 +0530

    Sort implementation - first order functions

--- Day-02/01-FunctionanlProgrammingTechniques/products.js
+++ Day-02/01-FunctionanlProgrammingTechniques/products.js
@@ -6,3 +6,56 @@ var products = [
     {id : 3, name : "Len", cost : 50, units : 30, category : 2},
     {id : 6, name : "Zen", cost : 70, units : 20, category : 1}
 ]
+
+var sort = function(list){
+    for(var i=0;i<list.length-1;i++)
+        for(var j=i+1;j<list.length;j++)
+            if (list[i].id > list[j].id){
+                var temp = list[i];
+                list[i]= list[j];
+                list[j] = temp;
+            }
+}
+
+console.log("Default list");
+console.table(products);
+
+console.log("After sorting [default - by id]");
+sort(products)
+console.table(products);
+
+var sort = function(list, attrName){
+    for(var i=0;i<list.length-1;i++)
+        for(var j=i+1;j<list.length;j++)
+            if (list[i][attrName] > list[j][attrName]){
+                var temp = list[i];
+                list[i]= list[j];
+                list[j] = temp;
+            }
+}
+
+console.log("After sorting [default - by cost]");
+sort(products,"cost")
+console.table(products);
+
+var sort = function(list, comparerFn){
+    for(var i=0;i<list.length-1;i++)
+        for(var j=i+1;j<list.length;j++)
+            if (comparerFn(list[i],list[j]) > 0){
+                var temp = list[i];
+                list[i]= list[j];
+                list[j] = temp;
+            }
+}
+
+var productComparerByValue = function(p1,p2){
+    var p1Value = p1.units * p1.cost,
+        p2Value = p2.units * p2.cost;
+    if (p1Value < p2Value) return -1;
+    if (p1Value > p2Value) return 1;
+    return 0;
+}
+
+console.log("After sorting by product value")
+sort(products,productComparerByValue);
+console.table(products);
